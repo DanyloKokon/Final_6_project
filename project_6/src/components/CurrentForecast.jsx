c577ac83d2b0fcc9bacca522e6cd409c822837e4
DanyloKokon <149526893+DanyloKokon@users.noreply.github.com>
2026-05-19 20:05:02 +0200

    сщььше

--- project_6/src/components/CurrentForecast.jsx
+++ project_6/src/components/CurrentForecast.jsx
@@ -9,7 +9,7 @@ function CurrentForecast({ currentWeather, location, setWhClicked }) {
     
     <ul className='current-forecast'>
         <li className="weather-card">
-             <h2 className="card-location">{location}</h2>
+             <h2 className="card-location">{currentWeather.location.name}</h2>
                 <p className="card-date">{currentWeather?.current?.last_updated}</p>
                 <img className="card-img" src={currentWeather?.current?.condition.icon} alt={currentWeather?.current?.condition.text} />
                 <p className="card-temp">{currentWeather?.current?.temp_c}°C</p>
@@ -17,9 +17,9 @@ function CurrentForecast({ currentWeather, location, setWhClicked }) {
         </li>
         {currentWeather?.forecast?.forecastday?.map((weather, index) => (
             <li className="weather-card" key={index}>
-                <h2 className="card-location">{location}</h2>
+                <h2 className="card-location">{currentWeather.location.name}</h2>
                 <p className="card-date">{weather.date}</p>
-                <img className="card-img" src={weather.day.condition.icon} alt={weather.day.condition.text} />
+                <img className="card-img" src={weather.day.condition.icon} alt={weather.forecast?.forecastday?.weather.condition.text} />
                 <p className="card-temp">{weather.day.maxtemp_c}°C</p>
                 <button className="card-btn" id={`detail-btn-${index}`} onClick={() => setWhClicked(index)}>Details</button>
             </li>
